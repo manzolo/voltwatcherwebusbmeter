bae8d3e27797ba987a0de64bbed7595168c8b07d
Andrea Manzi <manzolo@libero.it>
2019-09-26 22:00:47 +0200

    dimensione colonne

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -1,19 +1,116 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Cdf\BiCoreBundle\Controller\FiController;
 use Cdf\BiCoreBundle\Utils\Tabella\ParametriTabella;
+use Cdf\BiCoreBundle\Utils\Entity\EntityUtils;
 use App\Entity\Log;
 use App\Form\LogType;
-                
+
 /**
-* Log controller.
-*
-*/
+ * Log controller.
+ *
+ */
+class LogController extends FiController
+{
+    /**
+     * Lists all tables entities.
+     */
+    public function index(Request $request, Packages $assetsmanager)
+    {
+        $bundle = $this->getBundle();
+        $controller = $this->getController();
+        $idpassato = $request->get('id');
 
-class LogController extends FiController {
+        if (!$this->getPermessi()->canRead($this->getController())) {
+            throw new AccessDeniedException('Non si hanno i permessi per visualizzare questo contenuto');
+        }
+        $crudtemplate = $this->getCrudTemplate($bundle, $controller, $this->getThisFunctionName());
 
+        $entityclassnotation = $this->getEntityClassNotation();
+        $entityclass = $this->getEntityClassName();
+
+        $formclass = str_replace('Entity', 'Form', $entityclass);
+
+        $modellocolonne = array(
+            $controller . ".id" => array(
+                "nometabella" => $controller,
+                "nomecampo" => $controller . ".id",
+                "etichetta" => "Id",
+                "ordine" => 5,
+                "larghezza" => 50,
+                "escluso" => false
+            ),
+            $controller . ".data" => array(
+                "nometabella" => $controller,
+                "nomecampo" => $controller . ".data",
+                "etichetta" => "Rilevazione",
+                "ordine" => 20,
+                "larghezza" => 200,
+                "escluso" => false
+            ),
+            $controller . ".volt" => array(
+                "nometabella" => $controller,
+                "nomecampo" => $controller . ".volt",
+                "etichetta" => "Volt",
+                "ordine" => 30,
+                "larghezza" => 100,
+                "escluso" => false
+            ),
+            $controller . ".device" => array(
+                "nometabella" => $controller,
+                "nomecampo" => $controller . ".device",
+                "etichetta" => "Device",
+                "ordine" => 40,
+                "larghezza" => 200,
+                "escluso" => false
+            ),
+            $controller . ".temp" => array(
+                "nometabella" => $controller,
+                "nomecampo" => "Log.temp",
+                "etichetta" => "Temperatura",
+                "ordine" => 100,
+                "larghezza" => 100,
+                "escluso" => true
+            ),
+        );
+
+        $filtri = array();
+        $prefiltri = array();
+        $entityutils = new EntityUtils($this->get('doctrine')->getManager());
+        $tablenamefromentity = $entityutils->getTableFromEntity($entityclass);
+        $colonneordinamento = array($tablenamefromentity . '.id' => 'DESC');
+        $parametritabella = array('em' => ParametriTabella::setParameter('default'),
+            'tablename' => ParametriTabella::setParameter($tablenamefromentity),
+            'nomecontroller' => ParametriTabella::setParameter($controller),
+            'bundle' => ParametriTabella::setParameter($bundle),
+            'entityname' => ParametriTabella::setParameter($entityclassnotation),
+            'entityclass' => ParametriTabella::setParameter($entityclass),
+            'formclass' => ParametriTabella::setParameter($formclass),
+            'modellocolonne' => ParametriTabella::setParameter(json_encode($modellocolonne)),
+            'permessi' => ParametriTabella::setParameter(json_encode($this->getPermessi()->toJson($controller))),
+            'urltabella' => ParametriTabella::setParameter($assetsmanager->getUrl('/') . $controller . '/' . 'tabella'),
+            'baseurl' => ParametriTabella::setParameter($assetsmanager->getUrl('/')),
+            'idpassato' => ParametriTabella::setParameter($idpassato),
+            'titolotabella' => ParametriTabella::setParameter('Elenco ' . $controller),
+            'multiselezione' => ParametriTabella::setParameter('0'),
+            'editinline' => ParametriTabella::setParameter('0'),
+            'paginacorrente' => ParametriTabella::setParameter('1'),
+            'paginetotali' => ParametriTabella::setParameter(''),
+            'righetotali' => ParametriTabella::setParameter('0'),
+            'righeperpagina' => ParametriTabella::setParameter('15'),
+            'estraituttirecords' => ParametriTabella::setParameter('0'),
+            'colonneordinamento' => ParametriTabella::setParameter(json_encode($colonneordinamento)),
+            'filtri' => ParametriTabella::setParameter(json_encode($filtri)),
+            'prefiltri' => ParametriTabella::setParameter(json_encode($prefiltri)),
+            'traduzionefiltri' => ParametriTabella::setParameter(''),
+        );
+
+        return $this->render($crudtemplate, array('parametritabella' => $parametritabella));
+    }
 }
