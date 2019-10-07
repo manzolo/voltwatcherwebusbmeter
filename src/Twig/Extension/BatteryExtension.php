44ae4d6cc1031c7c7324311c7a5bf1388db688ca
Andrea Manzi <manzolo@libero.it>
2019-10-07 23:26:24 +0200

    improve frontend

--- src/Twig/Extension/BatteryExtension.php
+++ src/Twig/Extension/BatteryExtension.php
@@ -22,24 +22,24 @@ class BatteryExtension extends AbstractExtension {
         //Carica %	99	90	80  	70	60	50	40	30	20	10
         //Tensione	12,91 12,80 V	12,66 12,52  12,38           12,06   12,06  11,90 V 11,70 V
         if ($x >= 12.92)
-            return 100;
-        if ($x >= 12.80 && $x < 12.92)
+            return 100; //12,91
+        if ($x >= 12.80 && $x < 12.92) // 12,80
             return 90;
-        if ($x >= 12.66 && $x < 12.80)
+        if ($x >= 12.66 && $x < 12.80) //12,66
             return 80;
-        if ($x >= 12.52 && $x < 12.66)
+        if ($x >= 12.52 && $x < 12.66) //12,52
             return 70;
-        if ($x >= 12.66 && $x < 12.52)
+        if ($x >= 12.38 && $x < 12.52) //12,38
             return 60;
-        if ($x >= 12.52 && $x < 12.38)
+        if ($x >= 12.17 && $x < 12.38)
             return 50;
-        if ($x >= 12.38 && $x < 12.17)
+        if ($x >= 12.06 && $x < 12.17)
             return 40;
-        if ($x >= 12.17 && $x < 12.06)
+        if ($x >= 11.90 && $x < 12.06)
             return 30;
-        if ($x >= 12.06 && $x < 11.90)
+        if ($x >= 11.80 && $x < 11.90)
             return 20;
-        if ($x >= 11.90 && $x < 11.70)
+        if ($x >= 11.70 && $x < 11.80)
             return 10;
         if ($x < 11.70)
             return 0;
@@ -47,7 +47,7 @@ class BatteryExtension extends AbstractExtension {
 
     public function batteryClass($x) {
 
-        if (($this->batteryLevel($x)) >= 40) {
+        if (($this->batteryLevel($x)) >= 50) {
             return "";
         }
         if (($this->batteryLevel($x)) >= 30 && $this->batteryLevel($x) < 40) {
