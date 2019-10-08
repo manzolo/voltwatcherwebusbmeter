f8f757385f76e5fb673ed59c606cb79e2fe45600
Andrea Manzi <manzolo@libero.it>
2019-10-08 21:01:53 +0200

    fix device toString

--- src/Entity/Device.php
+++ src/Entity/Device.php
@@ -13,7 +13,7 @@ use App\Entity\BaseDevice;
 class Device extends BaseDevice {
 
     public function __toString() {
-        return $this->getName();
+        return ($this->getName()?$this->getName():$this->getAddress());
     }
 
 }
