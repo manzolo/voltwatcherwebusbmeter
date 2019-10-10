0075fbf735b4a8c4a359b7915a3537168ad2f03b
Andrea Manzi <manzolo@libero.it>
2019-10-11 00:14:45 +0200

    weekly report

--- src/Command/RiepilogoCommand.php
+++ src/Command/RiepilogoCommand.php
@@ -18,6 +18,7 @@ class RiepilogoCommand extends Command
     private $em;
     private $logger;
     private $mailer;
+private $templating;
 
     protected function configure()
     {
@@ -32,11 +33,12 @@ class RiepilogoCommand extends Command
                         false
         );
     }
-    public function __construct(ObjectManager $em, LoggerInterface $logger, \Swift_Mailer $mailer)
+    public function __construct(ObjectManager $em, LoggerInterface $logger, \Swift_Mailer $mailer, \Twig_Environment $templating)
     {
         $this->em = $em;
         $this->logger = $logger;
         $this->mailer = $mailer;
+        $this->templating = $templating;
         // you *must* call the parent constructor
         parent::__construct();
     }
@@ -44,6 +46,8 @@ class RiepilogoCommand extends Command
     {
         $date = (new \DateTime())->modify($this->journaldiffdays);
         $em = $this->em;
+
+        /* weekly */
         $qb = $em->createQueryBuilder('l')
                 ->select("d.id as deviceid, d.address as device, d.name as devicename, AVG(l.volt) avgvolt, MIN(l.volt) minvolt, MAX(l.volt) maxvolt")
                 ->from('App:Log', 'l')
@@ -69,10 +73,43 @@ class RiepilogoCommand extends Command
         $table
                 ->setHeaders(['Device', 'Min volt', 'Avg volt', 'Max volt'])
                 ->setRows($rows)
-                
+
         ;
         $table->render();
 
+        /* dayly */
+        $qb = $em->createQueryBuilder('l')
+                ->select("d.id as deviceid, d.address as device, d.name as devicename, SUBSTRING(l.data,1,10) AS day, AVG(l.volt) avgvolt, MIN(l.volt) minvolt, MAX(l.volt) maxvolt")
+                ->from('App:Log', 'l')
+                ->leftJoin('l.device', 'd')
+                ->andWhere('l.data >= :data')
+                ->setParameter("data", $date)
+                ->groupBy('l.device, day')
+                ->orderBy('l.device', "ASC")
+                ->addOrderBy('day', "DESC")
+                ->getQuery();
+
+        $riepilogodayrows = $qb->getResult();
+        $body = "<br/><br/><br/><br/>Dayly report<br/><br/>";
+        $rows = [];
+        foreach ($riepilogodayrows as $row) {
+            $deviceid = $row["deviceid"];
+            $day = \DateTime::createFromFormat("Y-m-d", $row["day"]);
+            $avg = round($row["avgvolt"], 2);
+            $min = round($row["minvolt"], 2);
+            $max = round($row["maxvolt"], 2);
+            $device = $em->getRepository("App:Device")->find($deviceid);
+            $body = $body . $device->__toString() . " Min:" . $min . " Avg:" . $avg . " Max:" . $max . "<br/><br/>";
+            $rows[] = [$day->format("d/m/Y"), $device->__toString(), $min, $avg, $max];
+        }
+
+        $table = new Table($output);
+        $table
+                ->setHeaders(['Day', 'Device', 'Min volt', 'Avg volt', 'Max volt'])
+                ->setRows($rows)
+
+        ;
+        $table->render();
         $sendmail = ($input->getOption('sendmail') !== false);
         if ($sendmail) {
 
@@ -81,7 +118,12 @@ class RiepilogoCommand extends Command
             $message = (new \Swift_Message("Energy report from " . $date->format("d/m/Y H:i:s") . " to " . (new \DateTime)->format("d/m/Y H:i:s")))
                     ->setFrom("voltwatcheralert@manzolo.it")
                     ->setTo($recipient)
-                    ->setBody($body, 'text/html')
+                    ->setBody($this->templating->render(
+                                    // templates/emails/registration.html.twig
+                                    'Report/index.html.twig',
+                                    ['rows' => $riepilogodayrows, "weeklyrows"=>$riepilogorows]
+                            ),
+                            'text/html')
                     // you can remove the following code if you don't define a text version for your emails
                     ->addPart($body, 'text/plain')
             ;
