66dca4adeb8e8b172ab3179d19ea2b1afb69e69c
Andrea Manzi <manzolo@libero.it>
2020-10-03 11:02:58 +0200

    improve api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @RouteResource("resources", pluralize=false)
+ * @RouteResource("api", pluralize=false)
  */
 class ApiController extends FOSRestController
 {
