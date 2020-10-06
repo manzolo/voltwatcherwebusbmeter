e701fdc12530e316d4caa6c5b5e779fd0ebbadb2
Andrea Manzi <manzolo@libero.it>
2020-10-06 20:53:13 +0200

    Add systemlog

---
+++ src/Entity/Systemlog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\BaseSystemlog;
+
+/**
+ * App\Entity\Systemlog
+ *
+ * @ORM\Entity()
+ */
+class Systemlog extends BaseSystemlog
+{
+}
