dcd4803ee9b2df8c9a029a943534adbb034f8fea
Andrea Manzi <andrea.manzi@comune.fi.it>
2020-11-23 01:00:25 +0100

    upgrade dependencies

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -148,6 +148,13 @@ class ApiController extends FOSRestController {
         return $this->view(['errcode' => 0, 'errmsg' => 'OK'], Response::HTTP_OK);
     }
 
+/**
+     * @ParamConverter("datavolt", class="array", converter="fos_rest.request_body")
+     */
+    public function postVoltRecordAction(array $datavolt) {
+        return $this->putVoltRecordAction($datavolt);
+    }    
+
     public function appGetSettingsAction() {
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder()
