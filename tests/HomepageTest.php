df6752f61ee3e994dc840ed923a8a22dea8c4b27
Andrea Manzi <manzolo@libero.it>
2021-12-01 19:32:31 +0100

    Improve tests

--- tests/HomepageTest.php
+++ tests/HomepageTest.php
@@ -11,9 +11,32 @@ class HomepageTest extends WebTestCase
 
     private $client;
 
-    public function setUp(): void
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    protected function setUp(): void
     {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()
+                ->get('doctrine')
+                ->getManager()
+        ;
         $this->client = static::createClient();
+    }
+    private function getUserFromUsername(string $username)
+    {
+
+        $users = $this->em->createQueryBuilder()
+                ->select('r')
+                ->from('BiCoreBundle:Operatori', 'r')
+                ->where('r.username = :username')
+                ->setParameter('username', $username)
+                ->getQuery()
+                ->getResult();
+        return $users[0];
     }
     private function getToken()
     {
@@ -35,22 +58,9 @@ class HomepageTest extends WebTestCase
     }
     private function getAdminClient()
     {
-        $container = $this->client->getContainer();
-        $session = $container->get('session');
 
-        /* @var $userManager \FOS\UserBundle\Doctrine\UserManager */
-        $userManager = $container->get('fos_user.user_manager');
-        /* @var $loginManager \FOS\UserBundle\Security\LoginManager */
-        $loginManager = $container->get('fos_user.security.login_manager');
-        $firewallName = $container->getParameter('fos_user.firewall_name');
-
-        $user = $userManager->findUserBy(array('username' => 'admin'));
-        $loginManager->loginUser($firewallName, $user);
-
-        /* save the login token into the session and put it in a cookie */
-        $container->get('session')->set('_security_' . $firewallName, serialize($container->get('security.token_storage')->getToken()));
-        $container->get('session')->save();
-        $this->client->getCookieJar()->set(new Cookie($session->getName(), $session->getId()));
+        $user = $this->getUserFromUsername("admin");
+        $this->client->loginUser($user);
     }
     public function testApiTokenWrongAuth()
     {
@@ -79,7 +89,7 @@ class HomepageTest extends WebTestCase
         $this->getAdminClient();
         $crawler = $this->client->request('GET', '/');
         $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString('San Piero a Sieve',  $this->client->getResponse()->getContent());
+        $this->assertStringContainsString('San Piero a Sieve', $this->client->getResponse()->getContent());
         //$crawler->html()
     }
 }
